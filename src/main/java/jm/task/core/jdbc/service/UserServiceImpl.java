6cbb03f42323e56d94f04b7331a37ebac69885b7
Doctrina42 <107268550+Doctrina42@users.noreply.github.com>
2023-09-05 19:02:01 +0700

    Add files via upload

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,40 @@
 package jm.task.core.jdbc.service;
 
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
 
+    private UserDao userDao = new UserDaoHibernateImpl();
+
+    public void createUsersTable() {
+        userDao.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        userDao.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        userDao.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
+        userDao.removeUserById(id);
 
     }
 
     public List<User> getAllUsers() {
-        return null;
+        List<User> allUsers = userDao.getAllUsers();
+        allUsers.forEach(System.out::println);
+        return allUsers;
     }
 
     public void cleanUsersTable() {
-
+        userDao.cleanUsersTable();
     }
 }
